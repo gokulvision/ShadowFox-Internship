cd3624991b97b91dd6a1daec7be9d1fa6b2f4d98
Gokul Lala <lalagokul01@gmail.com>
2026-04-25 22:13:12 +0530

    Added Task-1 variables

---
+++ Task-1/variables.py
@@ -0,0 +1,18 @@
+# Q1: Store 22/7 in variable pi and print its value and data type
+pi = 22 / 7
+print("Value of pi:", pi)
+print("Data type:", type(pi))
+
+
+# Q2: Trying to use 'for' as a variable (shows error because it's a keyword)
+# for = 4 This will cause SyntaxError
+print("\n'for' cannot be used as a variable name because it is a reserved keyword")
+
+
+# Q3: Calculate Simple Interest using P * R * T / 100
+principal = 1000
+rate = 5
+time = 3
+
+si = (principal * rate * time) / 100
+print("\nSimple Interest:", si)
